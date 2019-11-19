11fafa7535441a8a17b5afaa95873d32916a8e38
Rahul <rahul.dange1989@gmail.com>
2019-11-19 12:08:02 +0800

    - Created XCode Project. - Pod setup done. - Create initial UI.

---
+++ InmagineDemo/InmagineDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  InmagineDemo
+//
+//  Created by Rahul Dange on 11/19/19.
+//  Copyright © 2019 Rahul Dange. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Internal Methods -
+    
+    // MARK: - Event Handler Methods -
+    
+}
+
