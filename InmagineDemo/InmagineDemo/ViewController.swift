b1f956510a791181f1ad3265f974774c2b578b60
Rahul <rahul.dange1989@gmail.com>
2019-11-20 11:38:21 +0800

    - Added Clahe Filter.

--- InmagineDemo/InmagineDemo/ViewController.swift
+++ InmagineDemo/InmagineDemo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MetalPetal
 
 class ViewController: UIViewController {
     
@@ -16,14 +17,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var canvasSizeField: UITextField!
     
     var padding: CGFloat = 15.0 // -- Initial padding for 3x3
+    var imageView1: UIImageView? = nil
+    var imageView2: UIImageView? = nil
+    var imageView3: UIImageView? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.createImageViews()
         self.drawRectangles()
     }
     
     // MARK: - Internal Methods -
+    func createImageViews() {
+        self.imageView1 = self.createImageView()
+        self.imageView2 = self.createImageView()
+        self.imageView3 = self.createImageView()
+        for iv in [imageView1, imageView2, imageView3] {
+            self.canvasView.addSubview(iv!)
+        }
+    }
+    
     func drawRectangles() {
         var rectArray: [CGRect] = []
         let canvasLength = CGFloat(((self.canvasSizeField.text)! as NSString).floatValue)
@@ -45,6 +59,76 @@ class ViewController: UIViewController {
         
         self.canvasView.setRectanglesArray(rectArray: rectArray)
         self.canvasView.setNeedsDisplay()
+        self.imageView1?.frame = .init(
+                origin: .init(x: padding, y: padding),
+                size: .init(width: rectLength, height: rectLength)
+        )
+        self.imageView2?.frame = .init(
+            origin: .init(x: rectLength + (2.0 * padding), y: padding),
+                size: .init(width: rectLength, height: rectLength)
+        )
+        if totalRows == 3 {
+            self.imageView3?.frame = .init(
+                origin: .init(x: (2.0 * rectLength) + (3.0 * padding), y: padding),
+                    size: .init(width: rectLength, height: rectLength)
+            )
+        } else {
+            self.imageView3?.frame = .init(
+                origin: .init(x: padding, y: rectLength + (2.0 * padding)),
+                    size: .init(width: rectLength, height: rectLength)
+            )
+        }
+    }
+    
+    func createImageView() -> UIImageView {
+        let imageView = UIImageView.init()
+        imageView.image = UIImage.init(named: "apple")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }
+    
+    func applyVibranceFilter(image: CGImage, amount: CGFloat) {
+        DispatchQueue.global(qos: .background).async {
+            let mtImage = MTIImage.init(cgImage: image, options: [MTKTextureLoader.Option.SRGB : false])
+            let vibranceFilter = MTIVibranceFilter.init()
+            vibranceFilter.avoidsSaturatingSkinTones = true
+            vibranceFilter.inputImage = mtImage
+            vibranceFilter.amount = Float(amount)
+            
+            if let device = MTLCreateSystemDefaultDevice(),
+                let outputImage = vibranceFilter.outputImage {
+                do {
+                    let context = try MTIContext(device: device)
+                    let filteredImage = try context.makeCGImage(from: outputImage)
+                    DispatchQueue.main.async {
+                        self.imageView2?.image = UIImage(cgImage: filteredImage)
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    func applyClaheFilter(image: CGImage) {
+        DispatchQueue.global(qos: .background).async {
+            let mtImage = MTIImage.init(cgImage: image, options: [MTKTextureLoader.Option.SRGB : false])
+            let claheFilter = MTICLAHEFilter.init()
+            claheFilter.clipLimit = 2.0
+            claheFilter.inputImage = mtImage
+            if let device = MTLCreateSystemDefaultDevice(),
+                let outputImage = claheFilter.outputImage {
+                do {
+                    let context = try MTIContext(device: device)
+                    let filteredImage = try context.makeCGImage(from: outputImage)
+                    DispatchQueue.main.async {
+                       self.imageView3?.image = UIImage(cgImage: filteredImage)
+                    }
+                } catch {
+                    print(error)
+                }
+            }
+        }
     }
     
     // MARK: - Event Handler Methods -
@@ -76,6 +160,13 @@ class ViewController: UIViewController {
             navBtn.tag = 1
         }
         self.drawRectangles()
+    }
+    
+    @IBAction func applyFilters(_ sender: Any) {
+        // -- vibrance filter amount used is 2.0
+        self.applyVibranceFilter(image: (self.imageView1?.image?.cgImage)!, amount: 2.0)
+        // -- clahe filter
+        self.applyClaheFilter(image: (self.imageView1?.image?.cgImage)!)
     }
 }
 
